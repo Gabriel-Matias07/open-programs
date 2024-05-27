b4a433d966fb574fe602ae906c32a7e8e36a994d
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-27 08:28:10 -0300

    Discord add

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import time
 screenWidth, screenHeight = pyautogui.size() #Pega o tamanho do monitor principal
 print(screenWidth, screenHeight)
 
-currentMouseX, currentMouseY = pyautogui.position() #Pega as posições X e Y do mouse
-print(currentMouseX, currentMouseY)
+# currentMouseX, currentMouseY = pyautogui.position() #Pega as posições X e Y do mouse
+# print(currentMouseX, currentMouseY)
 
-currentMouseX, currentMouseY = 15, 1065
-print(currentMouseX, currentMouseY)
+currentMouseX, currentMouseY = 18, 1079
+# print(currentMouseX, currentMouseY)
 
 pyautogui.moveTo(currentMouseX, currentMouseY)
 time.sleep(2)
 
 pyautogui.click()
+pyautogui.write('Discord', interval=0.25)
+pyautogui.press('enter')
