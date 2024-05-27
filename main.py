6879469f88910dcc616f7a32fd38777eab449003
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-27 08:39:43 -0300

    Google Chrome add

--- main.py
+++ main.py
@@ -7,12 +7,25 @@ print(screenWidth, screenHeight)
 # currentMouseX, currentMouseY = pyautogui.position() #Pega as posições X e Y do mouse
 # print(currentMouseX, currentMouseY)
 
-currentMouseX, currentMouseY = 18, 1079
+# currentMouseX, currentMouseY = 18, 1079
 # print(currentMouseX, currentMouseY)
 
-pyautogui.moveTo(currentMouseX, currentMouseY)
-time.sleep(2)
+# pyautogui.moveTo(currentMouseX, currentMouseY)
+# time.sleep(2)
 
-pyautogui.click()
+#Discord:
+pyautogui.press('win')
+time.sleep(1)
 pyautogui.write('Discord', interval=0.25)
 pyautogui.press('enter')
+time.sleep(2)
+
+#Google Chrome
+pyautogui.press('win')
+time.sleep(1)
+pyautogui.write('Google Chrome', interval=0.24)
+pyautogui.press('enter')
+time.sleep(1)
+
+currentMouseX, currentMouseY = 865, 610
+pyautogui.click(currentMouseX, currentMouseY)
